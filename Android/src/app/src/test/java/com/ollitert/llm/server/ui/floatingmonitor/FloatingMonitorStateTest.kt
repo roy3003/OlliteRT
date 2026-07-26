d2a86599bb3d8e1e6682b68b257e72de686365f3
roy3003 <roy3003@users.noreply.github.com>
2026-07-26 13:27:17 +0800

    test: define running floating monitor state

---
+++ Android/src/app/src/test/java/com/ollitert/llm/server/ui/floatingmonitor/FloatingMonitorStateTest.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025-2026 @NightMean (https://github.com/NightMean)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ollitert.llm.server.ui.floatingmonitor
+
+import com.ollitert.llm.server.common.ServerStatus
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FloatingMonitorStateTest {
+
+  @Test
+  fun `running server without inference maps to running monitor`() {
+    assertEquals(
+      FloatingMonitorVisualState.Running,
+      deriveFloatingMonitorVisualState(
+        status = ServerStatus.RUNNING,
+        isInferring = false,
+      ),
+    )
+  }
+}
