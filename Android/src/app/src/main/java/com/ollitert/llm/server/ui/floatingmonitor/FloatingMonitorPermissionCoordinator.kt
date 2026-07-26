4033f206cea76450ba885f2ac358ae08412a7a2f
roy3003 <roy3003@users.noreply.github.com>
2026-07-26 14:53:02 +0800

    feat: add overlay permission suppression coordinator

---
+++ Android/src/app/src/main/java/com/ollitert/llm/server/ui/floatingmonitor/FloatingMonitorPermissionCoordinator.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025-2026 @NightMean (https://github.com/NightMean)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ollitert.llm.server.ui.floatingmonitor
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@Singleton
+class FloatingMonitorPermissionCoordinator @Inject constructor() {
+  private val _permissionFlowInProgress = MutableStateFlow(false)
+  val permissionFlowInProgress: StateFlow<Boolean> = _permissionFlowInProgress.asStateFlow()
+
+  fun beginPermissionFlow() {
+    _permissionFlowInProgress.value = true
+  }
+
+  fun endPermissionFlow() {
+    _permissionFlowInProgress.value = false
+  }
+}
