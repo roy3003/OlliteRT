e4f41dee34fd8d91470151714a56830256f6c9cf
roy3003 <roy3003@users.noreply.github.com>
2026-07-26 14:06:33 +0800

    test: require observable app foreground state

---
+++ Android/src/app/src/test/java/com/ollitert/llm/server/OlliteRTLifecycleProviderTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 Google LLC
+ * Modifications Copyright 2025-2026 @NightMean (https://github.com/NightMean)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ollitert.llm.server
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class OlliteRTLifecycleProviderTest {
+
+  @Test
+  fun `foreground state is observable and writable`() {
+    val provider = OlliteRTLifecycleProvider()
+
+    assertFalse(provider.isAppInForeground.value)
+
+    provider.setAppInForeground(true)
+
+    assertTrue(provider.isAppInForeground.value)
+  }
+}
